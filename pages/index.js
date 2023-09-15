60c4b45d6731f5cc2e270178a789a29979d703af
i-amdash <ashimidoyin@yahoo.com>
2023-09-15 17:05:02 +0100

    removed avatar

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ const Home = () => {
       exit="hidden"
       transition={{ duration: 1, ease: 'easeInOut'}}
       className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
-        <Avatar />
+        {/* <Avatar /> */}
       </motion.div>
     </div>
     </div>;
